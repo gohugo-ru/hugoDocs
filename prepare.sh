93d2728ae7c22941077b4a7d7c90ddbd2c8b04e2
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2020-11-19 10:25:39 +0300

    Обновил конфиг

--- prepare.sh
+++ prepare.sh
@@ -1,42 +1,14 @@
-#!/bin/sh
-# if [ "`git status -s`" ]
-# then
-#     echo "The working directory is dirty. Please commit any pending changes."
-#     exit 1;
-# fi
+mainBranch="master-ru"
+ghPagesBranch="gh-pages"
 
-echo "Deleting old publication"
-rm -rf out
-rm -rf _gh_pages
-mkdir _gh_pages
-git worktree prune
-rm -rf .git/worktrees/_gh_pages/
-
-echo "Checking out gh-pages branch into _gh_pages"
-git worktree add -B gh-pages _gh_pages origin/gh-pages
-
-echo "Removing existing files"
-rm -rf _gh_pages/*
-
-echo "Generating site"
-yarn export
-cp out/* _gh_pages/ -r
-echo tailwindcss.su >> _gh_pages/CNAME
-touch _gh_pages/.nojekyll
-
-echo "Updating gh-pages branch"
-
-# echo "Message:"
-# read message
-
-# if [$1 = '']
-# then
-# echo "Enter you message"
-# else
-# echo "You message: $message"
-# fi
-
-cd _gh_pages && git add --all && git commit -m "Добавил .nojekyll" && git push origin gh-pages && cd ..
-
-#echo "Pushing to github"
-#git push --all
+echo "Start prepare"
+echo "Checkout to $ghPagesBranch"
+git checkout --orphan $ghPagesBranch
+echo "Reset hard"
+git reset --hard
+echo "Initializing $ghPagesBranch branch"
+git commit --allow-empty -m "Initializing $ghPagesBranch branch"
+echo "Git push"
+git push origin $ghPagesBranch
+echo "Checkout to $mainBranch"
+git checkout $mainBranch
--- publish.sh
+++ publish.sh
@@ -1,42 +1,38 @@
-#!/bin/sh
-# if [ "`git status -s`" ]
-# then
-#     echo "The working directory is dirty. Please commit any pending changes."
-#     exit 1;
-# fi
+out="public"
+docs="_gh_pages"
+ghPagesBranch="gh-pages"
+site="gohugo.ru"
 
 echo "Deleting old publication"
-rm -rf out
-rm -rf _gh_pages
-mkdir _gh_pages
+rm -rf $docs
+mkdir $docs
 git worktree prune
-rm -rf .git/worktrees/_gh_pages/
+rm -rf .git/worktrees/$docs/
 
 echo "Checking out gh-pages branch into _gh_pages"
-git worktree add -B gh-pages _gh_pages origin/gh-pages
+git worktree add -B $ghPagesBranch $docs origin/$ghPagesBranch
 
 echo "Removing existing files"
-rm -rf _gh_pages/*
+rm -rf $docs/*
 
 echo "Generating site"
-yarn export
-cp out/* _gh_pages/ -r
-echo tailwindcss.su >> _gh_pages/CNAME
-touch _gh_pages/.nojekyll
+hugo
+cp $out/* $docs/ -r
+echo $site >> $docs/CNAME
+touch $docs/.nojekyll
 
 echo "Updating gh-pages branch"
 
-# echo "Message:"
-# read message
+read -p "Enter message for commit: " answer
 
-# if [$1 = '']
-# then
-# echo "Enter you message"
-# else
-# echo "You message: $message"
-# fi
+if [[ $answer ]]
+then
+message=$answer
+else
+message="Update docs"
+fi
 
-cd _gh_pages && git add --all && git commit -m "Добавил .nojekyll" && git push origin gh-pages && cd ..
+cd $docs && git add --all && git commit -m "$message" && git push origin $ghPagesBranch && cd ..
 
 #echo "Pushing to github"
 #git push --all
